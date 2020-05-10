ed8cc1b1b0781c353ca606a56de7b6d57cd6c917
David <david.camilo.toquica.cardenas@uqtr.ca>
2020-05-10 17:25:31 -0400

    define functions for the sensors

--- Sensores.py
+++ Sensores.py
@@ -1,54 +1,58 @@
+#%% Import Libraries
 import paho.mqtt.client as mqtt
-import time, psutil
-import argparse
+import time
+import psutil
 import json
-#from gpiozero import LEDBoard
 from time import sleep
 from w1thermsensor import W1ThermSensor
-import datetime
 import logging
 import mh_z19
-import aioschedule as schedule
-import asyncio
 
-
-teletopic = 'v1/devices/me/telemetry'
-try :
-  sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "011453ea62aa")
-except:
-  sensor = None
-
-logger.info("[{}] Starting infinit loop".format('RP'))
-#ADC = adc()
-
-
-while True:
-
- # loop.run_until_complete(schedule.run_pending())
-
-  if sensor is not None :
-    T1 = sensor.get_temperature()
+#%% Read Temperature
+def read_temp(sensor):
+  if temp_sensor is not None :
+    return sensor.get_temperature()
   else:
-    T1 = None
     logger.info("[{}] Not temp sensor : ".format('RP'))
-  #temps=psutil.sensors_temperatures()
+    return None
 
+#%% Read CO2
+def read_co2():
   CO = mh_z19.read()
   if CO is not None :
-    co2 = CO['co2']
+    return CO['co2']
   else:
-    co2 = None
     logger.info("[{}] Not CO2 sensor : ".format('RP'))
+    return None
 
-  #Isensor = ADC.getreading(0)
-  mem = psutil.virtual_memory()
-  data = {"ts":int(1000*time.time()),
-          "values":{"Temperature":T1,
-                    #"Power":mem.percent,
-                    #"Current":40.*Isensor,
-                    "CO2":co2,
-                    "RPmemory":round(mem.percent)
-                    #"pump":board[ligths_port[0]].value
-                    }
-          }
-  time.sleep()
+#%% Define client
+if __name__ == "__main__":
+  client = mqtt.Client()
+  client.connect('35.203.15.169', 1883, 60)
+  client.loop_start()
+  TOPIC = "sensors"
+
+#%% Start Logger
+  logging.basicConfig(level=logging.INFO, format='%(asctime)s  - [%(levelname)s] %(message)s')
+  logger = logging.getLogger(__name__)
+
+#%% Connect temperature sensor
+  try :
+    temp_sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "011453ea62aa")
+  except:
+    temp_sensor = None
+
+  logger.info("[{}] Starting infinit loop".format('RP')) #Registros gringos but it's ok
+
+#%% Send data
+  while True:
+    data = {"ts": int(1000*time.time()),
+            "values":{"Temperature": read_temp(temp_sensor),
+                      "CO2": read_co2(),
+                      "RPmemory": round(psutil.virtual_memory().percent)
+                      }
+            }
+
+    client.publish(TOPIC, json.dumps(data), 1)
+    time.sleep(5)
+
